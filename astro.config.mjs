7f0baa107542059b9081f9831c861ab20e184678
Ramzi Shodmonbekov <ramzi.shodmonbekov_2026@ucentralasia.org>
2025-11-30 23:16:06 +0600

    Configure site to be served at /ramzi-portfolio path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   output: 'static',
+  base: '/ramzi-portfolio',
   build: {
     assets: 'assets'
   },
